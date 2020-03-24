05dd19c1c670ee61badd674af59a3843c8dc7685
Mohd Faraz <mohd.faraz.abc@gmail.com>
2020-03-24 14:38:41 +0530

    sm8150-common: Update camera_motor hal
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- camera_motor/CameraMotor.h
+++ camera_motor/CameraMotor.h
@@ -16,10 +16,10 @@
 #ifndef VENDOR_LINEAGE_CAMERA_MOTOR_V1_0_FINGERPRINTINSCREEN_H
 #define VENDOR_LINEAGE_CAMERA_MOTOR_V1_0_FINGERPRINTINSCREEN_H
 
-#include <vendor/lineage/camera/motor/1.0/ICameraMotor.h>
+#include <vendor/one/camera/motor/1.0/ICameraMotor.h>
 
 namespace vendor {
-namespace lineage {
+namespace one {
 namespace camera {
 namespace motor {
 namespace V1_0 {
@@ -41,7 +41,7 @@ class CameraMotor : public ICameraMotor {
 }  // namespace V1_0
 }  // namespace motor
 }  // namespace camera
-}  // namespace lineage
+}  // namespace one
 }  // namespace vendor
 
 #endif  // VENDOR_LINEAGE_CAMERA_MOTOR_V1_0_FINGERPRINTINSCREEN_H
