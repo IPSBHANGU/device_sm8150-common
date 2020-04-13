AbShaheen184/android_device_oneplus_sm8150-common

Init omni DeviceParts from omni devices. Resolved all conflicts but still does not make compatible.
Squashed commit of the following:

commit 04b4170482806f4fb03df61367e76401a3e58846
Author: maxwen <max.weninger@gmail.com>
Date:   Wed Mar 4 00:35:41 2020 +0100

    oneplus7pro: add meta info

    Change-Id: Id0f34656105414a8ef37a4536ecbe9261af21b96

commit ef3c3f36c815680a89e6b172d5cea12cc0793b4c
Author: Marko Man <darkobas@gmail.com>
Date:   Thu Feb 20 20:04:36 2020 +0100

    op7fam: add back framebuffer config

    Change-Id: I8a04e229d1a223dd2dde45dd5a2243c4d9f20c19

commit 572d0c3f3ced4d021e990036000c8a7b9971f3c6
Author: Marko Man <darkobas@gmail.com>
Date:   Fri Feb 14 22:51:42 2020 +0100

    op7pro: update surfaceflinger props

    Change-Id: Ia55cf88e720e219791946d0d4416a711335375e2

commit d781ea936fd62cc605b86f5958c8b9cf16b0f44a
Author: Marko Man <darkobas@gmail.com>
Date:   Thu Feb 13 19:51:56 2020 +0100

    op7fam: update init with all fingerprints

    Change-Id: I375dd8805a15e424bb57004451e71e4e81814022

commit b822caf9a868ef2b280361ae1199e11241cc7cf7
Author: Marko Man <darkobas@gmail.com>
Date:   Fri Feb 14 22:44:05 2020 +0100

    op7fam: overlay: remove qcom flp and allow overlaying it

    enable inband hfp ringing

    Change-Id: Ibd967c63d20304c9883ae8aee06d498556c7efeb

commit a14960636844347c959ddc0d0e335e3d5ef503ca
Author: Marko Man <darkobas@gmail.com>
Date:   Mon Jan 27 22:26:55 2020 +0100

    op7pro: update blobs

    Change-Id: I52df49e69afa91a7e72776fe0db625d397ab99c5

commit 1b32de140bff4aa3b11a8de35c7e723d061c6d97
Author: Marko Man <darkobas@gmail.com>
Date:   Fri Feb 7 23:27:29 2020 +0100

    op7pro: change fod color

    Change-Id: Ia937e3de35d65e2e8a209b5bfa71e7acc22c7086

commit e36ec2f77640bdf3d566179f7b93452fdb77c1fc
Merge: 5d4a0608 91f3a78e
Author: Max Weninger <max.weninger@gmail.com>
Date:   Thu Feb 6 23:24:54 2020 +0100

    Merge "op7fam: use fake overlay to bind mount problematic vendor overlays" into android-10

commit 

DeviceParts/src/org/omnirom/device/RefreshRateTileService.java
@@ -1,0 +1,91 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.omnirom.device;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Icon;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+
+
+@TargetApi(24)
+public class RefreshRateTileService extends TileService {
+    private boolean enabled = false;
+    private boolean autoRefreshEnabled;
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        autoRefreshEnabled = Settings.System.getInt(this.getContentResolver(),
+                AutoRefreshRateSwitch.SETTINGS_KEY, 1) == 1;
+        if (autoRefreshEnabled) {
+            getQsTile().setState(Tile.STATE_UNAVAILABLE);
+        } else {
+            enabled = RefreshRateSwitch.isCurrentlyEnabled(this);
+            RefreshRateSwitch.setPeakRefresh(this, enabled);
+
+            getQsTile().setIcon(Icon.createWithResource(this,
+                    enabled ? R.drawable.ic_refresh_tile_90 : R.drawable.ic_refresh_tile_60));
+            getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        }
+        getQsTile().updateTile();
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        if (!autoRefreshEnabled) {
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+            enabled = RefreshRateSwitch.isCurrentlyEnabled(this);
+            RefreshRateSwitch.setPeakRefresh(this, enabled);
+            sharedPrefs.edit().putBoolean(DeviceSettings.KEY_REFRESH_RATE, enabled ? false : true).commit();
+            Settings.System.putFloat(this.getContentResolver(),
+                    Settings.System.PEAK_REFRESH_RATE, enabled ? 60f : 90f);
+            Settings.System.putFloat(this.getContentResolver(),
+                    Settings.System.MIN_REFRESH_RATE, enabled ? 60f : 90f);
+            getQsTile().setIcon(Icon.createWithResource(this,
+                    enabled ? R.drawable.ic_refresh_tile_60 : R.drawable.ic_refresh_tile_90));
+            getQsTile().setState(enabled ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
+            getQsTile().updateTile();
+        }
+    }
+}

IPSBHANGU/device_sm8150-common

OnePlusParts/src/com/one/device/RefreshRateTileService.java
@@ -1,0 +1,91 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.one.device;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Icon;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+
+
+@TargetApi(24)
+public class RefreshRateTileService extends TileService {
+    private boolean enabled = false;
+    private boolean autoRefreshEnabled;
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        autoRefreshEnabled = Settings.System.getInt(this.getContentResolver(),
+                AutoRefreshRateSwitch.SETTINGS_KEY, 1) == 1;
+        if (autoRefreshEnabled) {
+            getQsTile().setState(Tile.STATE_UNAVAILABLE);
+        } else {
+            enabled = RefreshRateSwitch.isCurrentlyEnabled(this);
+            RefreshRateSwitch.setPeakRefresh(this, enabled);
+
+            getQsTile().setIcon(Icon.createWithResource(this,
+                    enabled ? R.drawable.ic_refresh_tile_90 : R.drawable.ic_refresh_tile_60));
+            getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        }
+        getQsTile().updateTile();
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        if (!autoRefreshEnabled) {
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+            enabled = RefreshRateSwitch.isCurrentlyEnabled(this);
+            RefreshRateSwitch.setPeakRefresh(this, enabled);
+            sharedPrefs.edit().putBoolean(DeviceSettings.KEY_REFRESH_RATE, enabled ? false : true).commit();
+            Settings.System.putFloat(this.getContentResolver(),
+                    Settings.System.PEAK_REFRESH_RATE, enabled ? 60f : 90f);
+            Settings.System.putFloat(this.getContentResolver(),
+                    Settings.System.MIN_REFRESH_RATE, enabled ? 60f : 90f);
+            getQsTile().setIcon(Icon.createWithResource(this,
+                    enabled ? R.drawable.ic_refresh_tile_60 : R.drawable.ic_refresh_tile_90));
+            getQsTile().setState(enabled ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
+            getQsTile().updateTile();
+        }
+    }
+}