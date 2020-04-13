PeterCxy/android_device_oneplus_sm8150-common

sm8150: Import OmniRom DeviceParts
NurKeinNeid edits:
 - Migrate to androidx
 - Build in product
 - Reenable HBM
 - Remove HWK disabler
 - Modify overlays

Squashed the following changes:

op5: Keyhandler, default sim, fix derp …
@darkobas# darkobas committed on Nov 30, 2017
Oneplus3Parts: put that disgusting telephony stuff in a cage …
Merge "Oneplus5: Swap back and recent keys refactor" into android-9.0

op5(t): DeviceParts: use new torch API … @kldocGerrit Code Review @maxwen#
@darkobas maxwen authored and darkobas committed on Nov 29, 2017 @maxwen op5: tame
qti-telephony-common … @maxwen @darkobas maxwen authored and darkobas committed on
Nov 27, 2017 maxwen committed on Feb 27, 2018 Commits on Nov 18, 2017 op5: oreolo
blobolo … @darkobas darkobas committed on Nov 10, 2017 kldoc authored and Gerrit
Code Review committed on Jan 5 Commits on Oct 23, 2017 op5: create USB-Otg switch
… @darkobas darkobas committed on Oct 13, 2017 Commits on Feb 22, 2018 Commits on
Oct 13, 2017 op5: build DeviceParts with AAPT2 … @darkobas darkobas committed on
Oct 11, 2017 Automatic translation import … Commits on Oct 10, 2017 DeviceParts:
fix theme style … @maxwen @darkobas maxwen authored and darkobas committed on Oct
2, 2017

Commits on Oct 3, 2017 op5: enable swap back/recents … @darkobas darkobas
committed on Oct 3, 2017 @maxwen Oneplus3Parts: restore only enabled values …
@maxwen @darkobas maxwen authored and darkobas committed on Sep 30, 2017 maxwen
committed on Feb 22, 2018 op5: change Slider mode for O … @darkobas darkobas
committed on Sep 30, 2017 Oneplus5: Swap back and recent keys refactor … Commits
on Sep 11, 2017 op5: reeanble device parts … @darkobas darkobas committed on Sep
11, 2017 Commits on Feb 13, 2018 Commits on Sep 1, 2017 OP5 device parts:
offscreen gestures: load app icons on demand … @maxwen @darkobas maxwen authored
and darkobas committed on Aug 31, 2017 Merge "op5(t): DeviceParts: make sure we
always have an image" into a… … Commits on Aug 11, 2017 Device 

DeviceParts/src/org/omnirom/device/HBMModeSwitch.java
@@ -1,0 +1,53 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.omnirom.device;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import org.omnirom.device.DeviceSettings;
+
+public class HBMModeSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/sys/devices/virtual/graphics/fb0/hbm";
+
+    public static String getFile() {
+        if (Utils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Utils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        Utils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}

DeviceParts/src/org/omnirom/device/NightModeSwitch.java
@@ -1,0 +1,54 @@
+/*
+* Copyright (C) 2017 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.omnirom.device;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import org.omnirom.device.DeviceSettings;
+
+public class NightModeSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/sys/devices/virtual/graphics/fb0/night_mode";
+
+    public static String getFile() {
+        if (Utils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Utils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        Utils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}

DeviceParts/src/org/omnirom/device/PanelSettings.java
@@ -1,0 +1,110 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.omnirom.device;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.res.Resources;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.TwoStatePreference;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.util.Log;
+
+public class PanelSettings extends PreferenceFragment implements RadioGroup.OnCheckedChangeListener {
+    private RadioGroup mRadioGroup;
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mRadioGroup = (RadioGroup) view.findViewById(R.id.radio_group);
+        int checkedButtonId = R.id.off_mode;
+        if (NightModeSwitch.isCurrentlyEnabled(getContext())) {
+            checkedButtonId = R.id.night_mode;
+        } else if (DCIModeSwitch.isCurrentlyEnabled(getContext())) {
+            checkedButtonId = R.id.dci_mode;
+        } else if (SRGBModeSwitch.isCurrentlyEnabled(getContext())) {
+            checkedButtonId = R.id.srgb_mode;
+        }
+        mRadioGroup.check(checkedButtonId);
+        mRadioGroup.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.panel_modes, container, false);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        SharedPreferences.Editor edit = sharedPrefs.edit();
+        if (checkedId == R.id.srgb_mode) {
+            Utils.writeValue(DCIModeSwitch.getFile(), "0");
+            edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
+            Utils.writeValue(NightModeSwitch.getFile(), "0");
+            edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
+            Utils.writeValue(SRGBModeSwitch.getFile(), "1");
+            edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, true);
+        } else if (checkedId == R.id.dci_mode) {
+            Utils.writeValue(SRGBModeSwitch.getFile(), "0");
+            edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
+            Utils.writeValue(NightModeSwitch.getFile(), "0");
+            edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
+            Utils.writeValue(DCIModeSwitch.getFile(), "1");
+            edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, true);
+        } else if (checkedId == R.id.night_mode) {
+            Utils.writeValue(SRGBModeSwitch.getFile(), "0");
+            edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
+            Utils.writeValue(DCIModeSwitch.getFile(), "0");
+            edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
+            Utils.writeValue(NightModeSwitch.getFile(), "1");
+            edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, true);
+        } else if (checkedId == R.id.off_mode) {
+            Utils.writeValue(DCIModeSwitch.getFile(), "0");
+            edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
+            Utils.writeValue(NightModeSwitch.getFile(), "0");
+            edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
+            Utils.writeValue(SRGBModeSwitch.getFile(), "0");
+            edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
+        }
+        edit.commit();
+    }
+}

DeviceParts/src/org/omnirom/device/VibratorStrengthPreference.java
@@ -1,0 +1,110 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.omnirom.device;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+import android.database.ContentObserver;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.Button;
+import android.os.Bundle;
+import android.util.Log;
+import android.os.Vibrator;
+
+public class VibratorStrengthPreference extends Preference implements
+        SeekBar.OnSeekBarChangeListener {
+
+    private SeekBar mSeekBar;
+    private int mOldStrength;
+    private int mMinValue;
+    private int mMaxValue;
+    private Vibrator mVibrator;
+
+    private static final String FILE_LEVEL = "/sys/devices/virtual/timed_output/vibrator/vtg_level";
+    private static final long testVibrationPattern[] = {0,250};
+
+    public VibratorStrengthPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // from drivers/platform/msm/qpnp-haptic.c
+        // #define QPNP_HAP_VMAX_MIN_MV		116
+        // #define QPNP_HAP_VMAX_MAX_MV		3596
+        mMinValue = 116;
+        mMaxValue = 3596;
+
+        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        setLayoutResource(R.layout.preference_seek_bar);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+
+        mOldStrength = Integer.parseInt(getValue(getContext()));
+        mSeekBar = (SeekBar) holder.findViewById(R.id.seekbar);
+        mSeekBar.setMax(mMaxValue - mMinValue);
+        mSeekBar.setProgress(mOldStrength - mMinValue);
+        mSeekBar.setOnSeekBarChangeListener(this);
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(FILE_LEVEL);
+    }
+
+	public static String getValue(Context context) {
+		return Utils.getFileValue(FILE_LEVEL, "2700");
+	}
+
+	private void setValue(String newValue, boolean withFeedback) {
+	    Utils.writeValue(FILE_LEVEL, newValue);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putString(DeviceSettings.KEY_VIBSTRENGTH, newValue);
+        editor.commit();
+	    if (withFeedback) {
+            mVibrator.vibrate(testVibrationPattern, -1);
+        }
+	}
+
+    public static void restore(Context context) {
+        if (!isSupported()) {
+            return;
+        }
+
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(DeviceSettings.KEY_VIBSTRENGTH, "2700"); 
+        Utils.writeValue(FILE_LEVEL, storedValue);
+    }
+
+    public void onProgressChanged(SeekBar seekBar, int progress,
+            boolean fromTouch) {
+        setValue(String.valueOf(progress + mMinValue), true);
+    }
+
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // NA
+    }
+
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        // NA
+    }
+}
+

rootdir/Android.mk
@@ -222,6 +222,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.target.omni.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.target.omni.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC

IPSBHANGU/device_sm8150-common

OnePlusParts/src/com/one/device/HBMModeSwitch.java
@@ -1,0 +1,53 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.one.device;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import com.one.device.DeviceSettings;
+
+public class HBMModeSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/sys/devices/virtual/graphics/fb0/hbm";
+
+    public static String getFile() {
+        if (Utils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Utils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        Utils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}

OnePlusParts/src/com/one/device/NightModeSwitch.java
@@ -1,0 +1,54 @@
+/*
+* Copyright (C) 2017 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.one.device;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import com.one.device.DeviceSettings;
+
+public class NightModeSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/sys/devices/virtual/graphics/fb0/night_mode";
+
+    public static String getFile() {
+        if (Utils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Utils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        Utils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}

OnePlusParts/src/com/one/device/PanelSettings.java
@@ -1,0 +1,110 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.one.device;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.res.Resources;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.TwoStatePreference;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.util.Log;
+
+public class PanelSettings extends PreferenceFragment implements RadioGroup.OnCheckedChangeListener {
+    private RadioGroup mRadioGroup;
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mRadioGroup = (RadioGroup) view.findViewById(R.id.radio_group);
+        int checkedButtonId = R.id.off_mode;
+        if (NightModeSwitch.isCurrentlyEnabled(getContext())) {
+            checkedButtonId = R.id.night_mode;
+        } else if (DCIModeSwitch.isCurrentlyEnabled(getContext())) {
+            checkedButtonId = R.id.dci_mode;
+        } else if (SRGBModeSwitch.isCurrentlyEnabled(getContext())) {
+            checkedButtonId = R.id.srgb_mode;
+        }
+        mRadioGroup.check(checkedButtonId);
+        mRadioGroup.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.panel_modes, container, false);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        SharedPreferences.Editor edit = sharedPrefs.edit();
+        if (checkedId == R.id.srgb_mode) {
+            Utils.writeValue(DCIModeSwitch.getFile(), "0");
+            edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
+            Utils.writeValue(NightModeSwitch.getFile(), "0");
+            edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
+            Utils.writeValue(SRGBModeSwitch.getFile(), "1");
+            edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, true);
+        } else if (checkedId == R.id.dci_mode) {
+            Utils.writeValue(SRGBModeSwitch.getFile(), "0");
+            edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
+            Utils.writeValue(NightModeSwitch.getFile(), "0");
+            edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
+            Utils.writeValue(DCIModeSwitch.getFile(), "1");
+            edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, true);
+        } else if (checkedId == R.id.night_mode) {
+            Utils.writeValue(SRGBModeSwitch.getFile(), "0");
+            edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
+            Utils.writeValue(DCIModeSwitch.getFile(), "0");
+            edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
+            Utils.writeValue(NightModeSwitch.getFile(), "1");
+            edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, true);
+        } else if (checkedId == R.id.off_mode) {
+            Utils.writeValue(DCIModeSwitch.getFile(), "0");
+            edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
+            Utils.writeValue(NightModeSwitch.getFile(), "0");
+            edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
+            Utils.writeValue(SRGBModeSwitch.getFile(), "0");
+            edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
+        }
+        edit.commit();
+    }
+}

OnePlusParts/src/com/one/device/VibratorStrengthPreference.java
@@ -1,0 +1,110 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.one.device;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+import android.database.ContentObserver;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.Button;
+import android.os.Bundle;
+import android.util.Log;
+import android.os.Vibrator;
+
+public class VibratorStrengthPreference extends Preference implements
+        SeekBar.OnSeekBarChangeListener {
+
+    private SeekBar mSeekBar;
+    private int mOldStrength;
+    private int mMinValue;
+    private int mMaxValue;
+    private Vibrator mVibrator;
+
+    private static final String FILE_LEVEL = "/sys/devices/virtual/timed_output/vibrator/vtg_level";
+    private static final long testVibrationPattern[] = {0,250};
+
+    public VibratorStrengthPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // from drivers/platform/msm/qpnp-haptic.c
+        // #define QPNP_HAP_VMAX_MIN_MV		116
+        // #define QPNP_HAP_VMAX_MAX_MV		3596
+        mMinValue = 116;
+        mMaxValue = 3596;
+
+        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        setLayoutResource(R.layout.preference_seek_bar);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+
+        mOldStrength = Integer.parseInt(getValue(getContext()));
+        mSeekBar = (SeekBar) holder.findViewById(R.id.seekbar);
+        mSeekBar.setMax(mMaxValue - mMinValue);
+        mSeekBar.setProgress(mOldStrength - mMinValue);
+        mSeekBar.setOnSeekBarChangeListener(this);
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(FILE_LEVEL);
+    }
+
+	public static String getValue(Context context) {
+		return Utils.getFileValue(FILE_LEVEL, "2700");
+	}
+
+	private void setValue(String newValue, boolean withFeedback) {
+	    Utils.writeValue(FILE_LEVEL, newValue);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putString(DeviceSettings.KEY_VIBSTRENGTH, newValue);
+        editor.commit();
+	    if (withFeedback) {
+            mVibrator.vibrate(testVibrationPattern, -1);
+        }
+	}
+
+    public static void restore(Context context) {
+        if (!isSupported()) {
+            return;
+        }
+
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(DeviceSettings.KEY_VIBSTRENGTH, "2700"); 
+        Utils.writeValue(FILE_LEVEL, storedValue);
+    }
+
+    public void onProgressChanged(SeekBar seekBar, int progress,
+            boolean fromTouch) {
+        setValue(String.valueOf(progress + mMinValue), true);
+    }
+
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // NA
+    }
+
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        // NA
+    }
+}
+

rootdir/Android.mk
@@ -222,6 +222,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.target.one.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.target.one.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC