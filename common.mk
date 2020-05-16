864cee6c96a5eab89b2efc4ac40eaaeb22e2838c
IPSBHANGU <inderpreetbhangu2@gmail.com>
2020-05-16 20:35:44 +0530

    misc

--- common.mk
+++ common.mk
@@ -149,6 +149,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Dolby
+PRODUCT_PACKAGES += \
+    OPSoundTuner \
+    daxService
+
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl:64 \
