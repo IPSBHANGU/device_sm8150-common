PeterCxy/android_device_oneplus_sm8150-common

sm8150-common: Import camera_* from guacamole
* Now that we have more than 1 device using these
  apps there's no reason to have them in separate
  trees.

Change-Id: Ie5a969f2d40506a330eb721f7702424f1f9f087b

camera_helper/src/org/lineageos/camerahelper/BootCompletedReceiver.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright (c) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.camerahelper;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+    private static final String TAG = "OnePlusCameraHelper";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        Log.d(TAG, "Starting");
+        context.startService(new Intent(context, FallSensorService.class));
+    }
+}

camera_helper/src/org/lineageos/camerahelper/CameraMotorController.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (c) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.camerahelper;
+
+import android.os.FileUtils;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CameraMotorController {
+    private static final String TAG = "CameraMotorController";
+
+    // Camera motor paths
+    private static final String CAMERA_MOTOR_ENABLE_PATH =
+            "/sys/devices/platform/vendor/vendor:motor_pl/enable";
+    private static final String CAMERA_MOTOR_DIRECTION_PATH =
+            "/sys/devices/platform/vendor/vendor:motor_pl/direction";
+    private static final String CAMERA_MOTOR_POSITION_PATH =
+            "/sys/devices/platform/vendor/vendor:motor_pl/position";
+
+    // Motor control values
+    public static final String DIRECTION_DOWN = "0";
+    public static final String DIRECTION_UP = "1";
+    public static final String ENABLED = "1";
+    public static final String POSITION_DOWN = "1";
+    public static final String POSITION_UP = "0";
+
+    private CameraMotorController() {
+        // This class is not supposed to be instantiated
+    }
+
+    public static void setMotorDirection(String direction) {
+        try {
+            FileUtils.stringToFile(CAMERA_MOTOR_DIRECTION_PATH, direction);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to write to " + CAMERA_MOTOR_DIRECTION_PATH, e);
+        }
+    }
+
+    public static void setMotorEnabled() {
+        try {
+            FileUtils.stringToFile(CAMERA_MOTOR_ENABLE_PATH, ENABLED);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to write to " + CAMERA_MOTOR_ENABLE_PATH, e);
+        }
+    }
+
+    public static String getMotorPosition() {
+        try {
+            return FileUtils.readTextFile(new File(CAMERA_MOTOR_POSITION_PATH), 1, null);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to read to " + CAMERA_MOTOR_POSITION_PATH, e);
+        }
+        return null;
+    }
+}

camera_helper/src/org/lineageos/camerahelper/FallSensor.java
@@ -1,0 +1,118 @@
+/*
+ * Copyright (c) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.camerahelper;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.WindowManager;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class FallSensor implements SensorEventListener {
+    private static final boolean DEBUG = true;
+    private static final String TAG = "FallSensor";
+
+    private ExecutorService mExecutorService;
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private Context mContext;
+
+    public FallSensor(Context context) {
+        mContext = context;
+        mSensorManager = mContext.getSystemService(SensorManager.class);
+        mExecutorService = Executors.newSingleThreadExecutor();
+
+        for (Sensor sensor : mSensorManager.getSensorList(Sensor.TYPE_ALL)) {
+            if (DEBUG) Log.d(TAG, "Sensor type: " + sensor.getStringType());
+            if (TextUtils.equals(sensor.getStringType(), "oneplus.sensor.free_fall")) {
+                if (DEBUG) Log.d(TAG, "Found fall sensor");
+                mSensor = sensor;
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.values[0] <= 0) {
+            return;
+        }
+
+        Log.d(TAG, "Fall detected, ensuring front camera is closed");
+
+        // We shouldn't really bother doing anything if motor is already closed
+        if (CameraMotorController.getMotorPosition().equals(CameraMotorController.POSITION_DOWN)) {
+            return;
+        }
+
+        // Close the camera
+        CameraMotorController.setMotorDirection(CameraMotorController.DIRECTION_DOWN);
+        CameraMotorController.setMotorEnabled();
+
+        // Show alert dialog informing user that we closed the camera
+        new Handler(Looper.getMainLooper()).post(() -> {
+            AlertDialog alertDialog = new AlertDialog.Builder(mContext)
+                    .setTitle(R.string.free_fall_detected_title)
+                    .setMessage(R.string.free_fall_detected_message)
+                    .setNegativeButton(R.string.raise_the_camera, (dialog, which) -> {
+                        // Reopen the camera
+                        CameraMotorController.setMotorDirection(CameraMotorController.DIRECTION_UP);
+                        CameraMotorController.setMotorEnabled();
+                    })
+                    .setPositiveButton(R.string.close, (dialog, which) -> {
+                        // Go back to home screen
+                        Intent intent = new Intent(Intent.ACTION_MAIN);
+                        intent.addCategory(Intent.CATEGORY_HOME);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        mContext.startActivity(intent);
+                    })
+                    .create();
+            alertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+            alertDialog.setCanceledOnTouchOutside(false);
+            alertDialog.show();
+        });
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        /* Empty */
+    }
+
+    void enable() {
+        if (DEBUG) Log.d(TAG, "Enabling");
+        mExecutorService.submit(() -> {
+            mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        });
+    }
+
+    void disable() {
+        if (DEBUG) Log.d(TAG, "Disabling");
+        mExecutorService.submit(() -> {
+            mSensorManager.unregisterListener(this, mSensor);
+        });
+    }
+}

camera_helper/src/org/lineageos/camerahelper/FallSensorService.java
@@ -1,0 +1,77 @@
+/*
+ * Copyright (c) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.camerahelper;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.util.Log;
+
+public class FallSensorService extends Service {
+    private static final String TAG = "FallSensorService";
+    private static final boolean DEBUG = true;
+
+    private FallSensor mFallSensor;
+
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(Intent.ACTION_SCREEN_ON)) {
+                if (DEBUG) Log.d(TAG, "Screen on, enabling fall sensor");
+                mFallSensor.enable();
+            } else if (action.equals(Intent.ACTION_SCREEN_OFF)) {
+                if (DEBUG) Log.d(TAG, "Screen off, disabling fall sensor");
+                mFallSensor.disable();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        if (DEBUG) Log.d(TAG, "Creating service");
+        mFallSensor = new FallSensor(this);
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        intentFilter.addAction(Intent.ACTION_SCREEN_ON);
+        registerReceiver(mScreenStateReceiver, intentFilter);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        mFallSensor.enable();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (DEBUG) Log.d(TAG, "Destroying service");
+        mFallSensor.disable();
+        unregisterReceiver(mScreenStateReceiver);
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}

IPSBHANGU/device_sm8150-common

camera_helper/src/com/one/camerahelper/BootCompletedReceiver.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright (c) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.one.camerahelper;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+    private static final String TAG = "OnePlusCameraHelper";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        Log.d(TAG, "Starting");
+        context.startService(new Intent(context, FallSensorService.class));
+    }
+}

camera_helper/src/com/one/camerahelper/CameraMotorController.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (c) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.one.camerahelper;
+
+import android.os.FileUtils;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CameraMotorController {
+    private static final String TAG = "CameraMotorController";
+
+    // Camera motor paths
+    private static final String CAMERA_MOTOR_ENABLE_PATH =
+            "/sys/devices/platform/vendor/vendor:motor_pl/enable";
+    private static final String CAMERA_MOTOR_DIRECTION_PATH =
+            "/sys/devices/platform/vendor/vendor:motor_pl/direction";
+    private static final String CAMERA_MOTOR_POSITION_PATH =
+            "/sys/devices/platform/vendor/vendor:motor_pl/position";
+
+    // Motor control values
+    public static final String DIRECTION_DOWN = "0";
+    public static final String DIRECTION_UP = "1";
+    public static final String ENABLED = "1";
+    public static final String POSITION_DOWN = "1";
+    public static final String POSITION_UP = "0";
+
+    private CameraMotorController() {
+        // This class is not supposed to be instantiated
+    }
+
+    public static void setMotorDirection(String direction) {
+        try {
+            FileUtils.stringToFile(CAMERA_MOTOR_DIRECTION_PATH, direction);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to write to " + CAMERA_MOTOR_DIRECTION_PATH, e);
+        }
+    }
+
+    public static void setMotorEnabled() {
+        try {
+            FileUtils.stringToFile(CAMERA_MOTOR_ENABLE_PATH, ENABLED);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to write to " + CAMERA_MOTOR_ENABLE_PATH, e);
+        }
+    }
+
+    public static String getMotorPosition() {
+        try {
+            return FileUtils.readTextFile(new File(CAMERA_MOTOR_POSITION_PATH), 1, null);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to read to " + CAMERA_MOTOR_POSITION_PATH, e);
+        }
+        return null;
+    }
+}

camera_helper/src/com/one/camerahelper/FallSensor.java
@@ -1,0 +1,118 @@
+/*
+ * Copyright (c) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.one.camerahelper;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.WindowManager;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class FallSensor implements SensorEventListener {
+    private static final boolean DEBUG = true;
+    private static final String TAG = "FallSensor";
+
+    private ExecutorService mExecutorService;
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private Context mContext;
+
+    public FallSensor(Context context) {
+        mContext = context;
+        mSensorManager = mContext.getSystemService(SensorManager.class);
+        mExecutorService = Executors.newSingleThreadExecutor();
+
+        for (Sensor sensor : mSensorManager.getSensorList(Sensor.TYPE_ALL)) {
+            if (DEBUG) Log.d(TAG, "Sensor type: " + sensor.getStringType());
+            if (TextUtils.equals(sensor.getStringType(), "oneplus.sensor.free_fall")) {
+                if (DEBUG) Log.d(TAG, "Found fall sensor");
+                mSensor = sensor;
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.values[0] <= 0) {
+            return;
+        }
+
+        Log.d(TAG, "Fall detected, ensuring front camera is closed");
+
+        // We shouldn't really bother doing anything if motor is already closed
+        if (CameraMotorController.getMotorPosition().equals(CameraMotorController.POSITION_DOWN)) {
+            return;
+        }
+
+        // Close the camera
+        CameraMotorController.setMotorDirection(CameraMotorController.DIRECTION_DOWN);
+        CameraMotorController.setMotorEnabled();
+
+        // Show alert dialog informing user that we closed the camera
+        new Handler(Looper.getMainLooper()).post(() -> {
+            AlertDialog alertDialog = new AlertDialog.Builder(mContext)
+                    .setTitle(R.string.free_fall_detected_title)
+                    .setMessage(R.string.free_fall_detected_message)
+                    .setNegativeButton(R.string.raise_the_camera, (dialog, which) -> {
+                        // Reopen the camera
+                        CameraMotorController.setMotorDirection(CameraMotorController.DIRECTION_UP);
+                        CameraMotorController.setMotorEnabled();
+                    })
+                    .setPositiveButton(R.string.close, (dialog, which) -> {
+                        // Go back to home screen
+                        Intent intent = new Intent(Intent.ACTION_MAIN);
+                        intent.addCategory(Intent.CATEGORY_HOME);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        mContext.startActivity(intent);
+                    })
+                    .create();
+            alertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+            alertDialog.setCanceledOnTouchOutside(false);
+            alertDialog.show();
+        });
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        /* Empty */
+    }
+
+    void enable() {
+        if (DEBUG) Log.d(TAG, "Enabling");
+        mExecutorService.submit(() -> {
+            mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        });
+    }
+
+    void disable() {
+        if (DEBUG) Log.d(TAG, "Disabling");
+        mExecutorService.submit(() -> {
+            mSensorManager.unregisterListener(this, mSensor);
+        });
+    }
+}

camera_helper/src/com/one/camerahelper/FallSensorService.java
@@ -1,0 +1,77 @@
+/*
+ * Copyright (c) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.one.camerahelper;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.util.Log;
+
+public class FallSensorService extends Service {
+    private static final String TAG = "FallSensorService";
+    private static final boolean DEBUG = true;
+
+    private FallSensor mFallSensor;
+
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(Intent.ACTION_SCREEN_ON)) {
+                if (DEBUG) Log.d(TAG, "Screen on, enabling fall sensor");
+                mFallSensor.enable();
+            } else if (action.equals(Intent.ACTION_SCREEN_OFF)) {
+                if (DEBUG) Log.d(TAG, "Screen off, disabling fall sensor");
+                mFallSensor.disable();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        if (DEBUG) Log.d(TAG, "Creating service");
+        mFallSensor = new FallSensor(this);
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        intentFilter.addAction(Intent.ACTION_SCREEN_ON);
+        registerReceiver(mScreenStateReceiver, intentFilter);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        mFallSensor.enable();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (DEBUG) Log.d(TAG, "Destroying service");
+        mFallSensor.disable();
+        unregisterReceiver(mScreenStateReceiver);
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}