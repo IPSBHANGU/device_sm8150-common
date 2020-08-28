6f82154d87bb99bd9dc767d60fbe5ce369db7e69
IPSBHANGU <inderpreetbhangu2@gmail.com>
2020-08-28 20:23:52 +0530

    rebase on pa implementation

--- fod/service.cpp
+++ fod/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "one.biometrics.fingerprint.inscreen@1.0-service.oneplus_msmnile"
+#define LOG_TAG "vendor.one.biometrics.fingerprint.inscreen@1.0-service.oneplus_msmnile"
 
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
